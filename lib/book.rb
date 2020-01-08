f17568bc0968fb961bb85f11c279b80a96971088
S. Makai Takori <makait87@gmail.com>
2020-01-08 16:22:02 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -8,7 +8,11 @@ class Book
   end
   
   def turn_page
+<<<<<<< HEAD
     puts "Flipping the page...wow, you read fast!"
+=======
+    puts "can turn the page"
+>>>>>>> f314d9b09164c8ccf7cc0583d6716ec656811f3c
   end  
 
 end  
